0776325c396e045f23bd21fc3fb3b79590575f6c
Adil Rahmani <didilusse@gmail.com>
2024-11-23 13:32:36 -0500

    Added placeholder view models

---
+++ ToDoList/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  ToDoList
+//
+//  Created by Adil Rahmani on 11/23/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
