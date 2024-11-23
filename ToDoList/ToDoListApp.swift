9abfb23a684cdb56cbdb3795cbd1959aa72123b4
Adil Rahmani <didilusse@gmail.com>
2024-11-22 20:29:21 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Adil Rahmani on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
